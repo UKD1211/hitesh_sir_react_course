5b1599e564dc1e99b0af32f6fc53c1b55660d460
UTTAM <duttau326@gmail.com>
2023-09-26 01:40:14 +0530

    2nd vid of HC

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+
+
+
+function App() {
+  return (
+    <h1>Hello uttam go for it babe</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+
+
+function App() {
+  
+
+  return (
+    <h1>Uttam dutta lets go with vite | UKD</h1>
+  )
+}
+
+export default App
